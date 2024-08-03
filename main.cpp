169eb1333f961f9b5608edb14d0e0fb5dd6f0eb3
Ferdinand <atiferdinand31@gmail.com>
2024-08-03 13:21:24 +0100

    deuxieme modifictaion

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 
 int main()
 {
-    unsigned char b = 0b0001;
+    unsigned char b = 0b0001 , a = 0b0011;
     bool c;
     std::cout << "hello first repositry "<<std::endl;
     c = b & (1<<0);
+    if  (a & (1<<2))
+      std::cout <<"le bit  à cette position est à 1"<<std::endl;
+    else 
+      std::cout <<"le bit  à cette position est à 0"<<std::endl;
     std::cout << "la valeur de c = "<<c<<std::endl;
     return 0 ;
 }
