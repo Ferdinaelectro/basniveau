1671c0b50685e28c2167724c7e3122915471a929
Ferdinand <atiferdinand31@gmail.com>
2024-08-03 13:15:12 +0100

    Description des modifications apportées à main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
+    unsigned char b = 0b0001;
+    bool c;
     std::cout << "hello first repositry "<<std::endl;
+    c = b & (1<<0);
+    std::cout << "la valeur de c = "<<c<<std::endl;
     return 0 ;
 }
